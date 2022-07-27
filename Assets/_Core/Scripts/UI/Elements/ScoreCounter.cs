de9b98aeba9380bf78e993fd0ba81c1e640307f5
Fyodar Krasin <fyodar.krasin@gmail.com>
2022-07-27 19:06:04 +0300

    changes

---
+++ Assets/_Core/Scripts/UI/Elements/ScoreCounter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DG.Tweening;
+using TapSlotsTest.Managers;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+using Zenject.Asteroids;
+
+namespace TapSlotsTest.UI.Elements
+{
+    public class ScoreCounter : MonoBehaviour
+    {
+        [Inject] private StatisticsManager _statistics;
+        
+        [SerializeField] private ScoreCounterPositions _scoreCounterPositions;
+        [SerializeField] private RectTransform _firstDigitScroll;
+        [SerializeField] private RectTransform _secondDigitScroll;
+
+        private void OnEnable()
+        {
+            _firstDigitScroll.DOMoveY(_scoreCounterPositions.Zero, 1);
+            // _secondDigitScroll.SetY(_scoreCounterPositions.Zero);
+            _firstDigitScroll.position.Set(0, _scoreCounterPositions.Zero,0 );
+        }
+    }
+}
---
+++ Assets/_Core/Scripts/UI/Elements/ScoreCounterPositions.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace TapSlotsTest.UI.Elements
+{
+    [CreateAssetMenu(fileName = "ScrollCounterPositions", menuName = "ScriptableObjects/ScrollCounterPositions")]
+    public class ScoreCounterPositions : ScriptableObject
+    {
+        [SerializeField] private float _zero;
+        [SerializeField] private float _one;
+        [SerializeField] private float _two;
+        [SerializeField] private float _three;
+        [SerializeField] private float _four;
+        [SerializeField] private float _five;
+        [SerializeField] private float _six;
+        [SerializeField] private float _seven;
+        [SerializeField] private float _eight;
+        [SerializeField] private float _nine;
+        
+        public float Zero => _zero;
+        public float One => _one;
+        public float Two => _two;
+        public float Three => _three;
+        public float Four => _four;
+        public float Five => _five;
+        public float Six => _six;
+        public float Seven => _seven;
+        public float Eight => _eight;
+        public float Nine => _nine;
+    }
+}
